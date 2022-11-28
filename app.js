757aee061d334873c3fa9e772a1607797d4861fc
Alice <alicewang.km@gmail.com>
2022-11-28 12:37:10 +1100

    move task input page into boostrap  modal

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Task class:represents a task
 setInterval(show, 1000);
 function show() {
 let  date = new Date();
@@ -7,127 +8,142 @@ let  date = new Date();
 	document.getElementById("show").innerHTML = date_time;
 };
 
-
-const tasknameEl = document.getElementById("fname")
-const assigninputEL = document.getElementById("assign-input")
-const descriptionEl = document.getElementById("description")
-const form = document.getElementById("taskinput")
-
-form.addEventListener('click' , function (e) {
-    e.preventDefault()
-    let isNameValid = checkUsername(),
-        isDateValid = checkDueDate(),
-        isAssignValid = checkAssignTo(),
-        isDescriptionValid = checkDescription();
-    
-    let isFormValid = isNameValid &&
-        isDateValid&&
-        isAssignValid&&
-        isDescriptionValid;
-
-    if(isDescriptionValid) {
-
+class Task {
+    constructor(name,description,assignedto,duedate,status){
+        this.name = name;
+        this.description = description;
+        this.assignedto = assignedto;
+        this.duedate = duedate;
+        this.status = status;
     }
-
-
-
-
-})
-
-const isRequired = value => value ===  '' ? false : true;
-const isBetween = (length, min, max) => length < min || length > max ? false : true;
-
-const showError = (input, message) => {
-    // get the form-field element
-    const formField = input.parentElement;
-    // add the error class
-    formField.classList.remove('success');
-    formField.classList.add('error');
-
-    // show the error message
-    const error = formField.querySelector('small');
-    error.textContent = message;
-};
-const showSuccess = (input) => {
-    // get the form-field element
-    const formField = input.parentElement;
-
-    // remove the error class
-    formField.classList.remove('error');
-    formField.classList.add('success');
-
-    // hide the error message
-    const error = formField.querySelector('small');
-    error.textContent = '';
 }
 
-
-const checkUsername = () => {
-
-    let valid = false;
-    const min = 1,
-        max = 8;
-    const username = tasknameEl.value.trim();
-
-    if (!isRequired(username)) {
-        showError(tasknameEl, 'Name cannot be blank.');
-    } else if (!isBetween(username.length, min, max)) {
-        showError(tasknameEl, `Name must be between ${min} and ${max} characters.`)
-    } else {
-        showSuccess(tasknameEl);
-        valid = true;
+// UI Class : handle UI tasks
+class UI {
+    static displayTask() {
+        // const StorageTasks = [
+        // {
+        //     name: "Alice",
+        //     description: "buy new tyres",
+        //     assignedto:"Alice2",
+        //     duedate: "2022/12/01",
+        //     status: "todo"
+        // },
+        // {
+        //     name: "John",
+        //     description: "mop floor",
+        //     assignedto:"John2",
+        //     duedate: "2022/12/10",
+        //     status: "todo"
+        // }
+        // ]
+        const tasks = Store.getTasks();
+    // const tasks = StorageTasks
+    tasks.forEach((task)=> UI.addTaskToList(task));
     }
-    return valid;
-}
 
-const checkDescription = () => {
-
-    let valid = false;
-    const min = 1,
-        max = 15;
-    const description = descriptionEl.value.trim();
-
-    if (!isRequired(description)) {
-        showError(descriptionEl, 'Description cannot be blank.');
-    } else if (!isBetween(description.length, min, max)) {
-        showError(descriptionEl, `Description must be between ${min} and ${max} characters.`)
-    } else {
-        showSuccess(descriptionEl);
-        valid = true;
+    static addTaskToList(task){
+        const list = document.getElementById("task-list")
+        const row = document.createElement('tr')
+        row.innerHTML = `
+        <td>${task.name}</td>
+        <td>${task.description}</td>
+        <td>${task.assignedto}</td>
+        <td>${task.duedate}</td>
+        <td>${task.status}</td>
+        <td><a href="#" class ="btn btn-outline-danger btn-sm delete">Delete</a></td>
+        <td><a href="#" class ="btn btn-outline-info btn-sm edit">Edit</a></td>
+        `;
+        list.appendChild(row)
     }
-    return valid;
-}
-
-const checkAssignTo = () => {
-
-    let valid = false;
-    const min = 1,
-        max = 8;
-    const assign = assigninputEL.value.trim();
-
-    if (!isRequired(assign)) {
-        showError(assigninputEL, 'Cannot be blank.');
-    } else if (!isBetween(assign.length, min, max)) {
-        showError(assigninputEL, `Must be between ${min} and ${max} characters.`)
-    } else {
-        showSuccess(assigninputEL);
-        valid = true;
+    static deleteTask(el) {
+        if(el.classList.contains("delete")) {
+            el.parentElement.parentElement.remove();
+        }
     }
-    return valid;
-}
+    static editTask(el){
+        const tasks = Store.getTasks();
+        if(el.classList.contains("edit")){
+            console.log(tasks);
+        }
+        
+    }
+    static showAlert(message,className){
+        const div = document.createElement("div");
+        div.className =`alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const dady = document.getElementById('m-content')
+        const body =document.getElementById('container')
+        dady.insertBefore(div,body);
+        
+        setTimeout(()=>document.querySelector('.alert').remove(),3000);
 
-
-const checkDueDate = () => {
-    
-    let UserDate = document.getElementById("dueDate").value;
-    let ToDate = new Date();
-
-    if (new Date(UserDate).getTime() <= ToDate.getTime()) {
-        alert('The Date must be Bigger or Equal to today.');
-        return false
     }  
-       return true;  
+    static clearFields(){
+        document.getElementById("fname").value = '';
+        document.getElementById("assign-input").value = '';
+        document.getElementById("description").value = '';
+        document.getElementById("dueDate").value = '';
+        document.getElementById("status-input").value = '';
+        
     }
-   
+}
+// Store Class : handle Storage
+class Store {
+    static getTasks() {
+        let tasks;
+        if(localStorage.getItem('tasks') === null) {
+            tasks = [];
+        } else {
+            tasks = JSON.parse(localStorage.getItem('tasks'));
+        }
+        return tasks
+    }
+    static addTask(task){
+        const tasks = Store.getTasks();
+        tasks.push(task);
+        localStorage.setItem('tasks',JSON.stringify(tasks));
 
-   
+    }
+    static removeTask(status){
+        const tasks = Store.getTasks();
+        tasks.forEach((task,index)=>{
+            if(task.status === status){
+                tasks.splice(index, 1)
+            }
+        })
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+}
+// Event: Display Task
+document.addEventListener('DOMContentLoaded', UI.displayTask);
+// Event: Add a Task
+document.getElementById('task-input').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    // Get form values
+    const name = document.getElementById("fname").value;
+    const description = document.getElementById("description").value;
+    const assignedto = document.getElementById("assign-input").value;
+    const duedate = document.getElementById("dueDate").value;
+    const status = document.getElementById("status-input").value;
+
+    // Validate 
+    if(name === ""||description === ""||assignedto === ""||status === ""){
+        UI.showAlert('Please fill in all fields','danger')
+    }else{
+        // Instatiate book
+    const task = new Task(name,description,assignedto,duedate,status);
+    
+    UI.addTaskToList(task);
+    Store.addTask(task)
+    UI.showAlert('Task Added','success');
+    UI.clearFields();
+    }
+ })
+// Event: Remove a Task
+    document.getElementById("task-list").addEventListener("click",(e) =>{
+    UI.deleteTask(e.target)
+    Store.removeTask(e.target.parentElement.previousElementSibling.textContent)
+    UI.editTask(e.target)
+    });
+
---
+++ validation.js
@@ -0,0 +1,123 @@
+
+
+const tasknameEl = document.getElementById("fname");
+const assigninputEL = document.getElementById("assign-input");
+const descriptionEl = document.getElementById("description");
+
+const form = document.getElementById("task-input");
+
+form.addEventListener("click", (e)=> {
+    e.preventDefault(); 
+
+    let isNameValid = checkUsername(),
+        isDateValid = checkDueDate(),
+        isAssignValid = checkAssignTo(),
+        isDescriptionValid = checkDescription();
+    
+    let isFormValid = isNameValid &&
+        isDateValid&&
+        isAssignValid&&
+        isDescriptionValid;
+
+    if(isFormValid) {
+
+    }
+});
+
+const isRequired = value => value ===  '' ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+const showError = (input, message) => {
+    // get the form-field element
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    // show the error message
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input.parentElement;
+
+    // remove the error class
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    // hide the error message
+    const error = formField.querySelector('small');
+    error.textContent = '';
+}
+
+
+const checkUsername = () => {
+
+    let valid = false;
+    const min = 1,
+        max = 8;
+    const username = tasknameEl.value.trim();
+
+    if (!isRequired(username)) {
+        showError(tasknameEl, 'Name cannot be blank.');
+    } else if (!isBetween(username.length, min, max)) {
+        showError(tasknameEl, `Name must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(tasknameEl);
+        valid = true;
+    }
+    return valid;
+}
+
+const checkDescription = () => {
+
+    let valid = false;
+    const min = 1,
+        max = 15;
+    const description = descriptionEl.value.trim();
+
+    if (!isRequired(description)) {
+        showError(descriptionEl, 'Description cannot be blank.');
+    } else if (!isBetween(description.length, min, max)) {
+        showError(descriptionEl, `Description must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(descriptionEl);
+        valid = true;
+    }
+    return valid;
+}
+
+const checkAssignTo = () => {
+
+    let valid = false;
+    const min = 1,
+        max = 8;
+    const assign = assigninputEL.value.trim();
+
+    if (!isRequired(assign)) {
+        showError(assigninputEL, 'Cannot be blank.');
+    } else if (!isBetween(assign.length, min, max)) {
+        showError(assigninputEL, `Must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(assigninputEL);
+        valid = true;
+    }
+    return valid;
+}
+
+
+const checkDueDate = () => {
+    
+    let UserDate = document.getElementById("dueDate").value;
+    let ToDate = new Date();
+
+    if (new Date(UserDate).getTime() <= ToDate.getTime()) {
+        alert('The Date must be Bigger or Equal to today.');
+        return false
+    }  
+       return true;  
+    }
+
+   
+    
